1113b9407f33dc6280766111c4d85c9b16850dea
jsilvam <jsilvam87@gmail.com>
2021-09-04 19:26:36 -0500

    lab1

---
+++ Lab1/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Automovil auto = new Automovil(100, 10, 0.9);
+        auto.abastecer(90);
+        auto.abastecer(100);
+        auto.viajar(4);
+
+        Camion camion = new Camion(100, 10, 0.9);
+        camion.abastecer(90);
+        camion.abastecer(100);
+        camion.viajar(12);
+    }
+}
